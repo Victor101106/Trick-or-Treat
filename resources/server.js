0570551162227aeeb9056d1b298c36afee3633b6
Victor101106 <victorgabriel101106+github@gmail.com>
2023-10-20 11:07:18 -0300

    chore: add .env support

--- resources/server.js
+++ resources/server.js
@@ -2,13 +2,16 @@ import es6Renderer from 'express-es6-template-engine'
 import cookieParser from 'cookie-parser'
 import { routes } from './routes.js'
 import { fileURLToPath } from 'url';
+import { config } from 'dotenv';
 import { dirname } from 'path';
 import express from 'express'
 
+config()
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const PORT = process.env.PORT
 const app = express()
-const PORT = '3030'
 
 app.engine('html', es6Renderer)
 app.set('view engine', 'html')
